92390198c6b848e20b836b3608bda199d72b36a5
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-06-12 11:45:33 +0100

    added execute.c

---
+++ execute.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * execute - execute the command
+ * @cmd: command given by the user
+ * Return: 1
+ */
+int execute(char **cmd)
+{
+	pid_t pid, wpid;
+	int status;
+	char **envp = environ;
+
+	pid = fork();
+	if (pid < 0)
+	{
+		perror("Error: unable to fork parent");
+	}
+	else if (pid == 0)
+	{
+		if (execve(cmd[0], parsed, env) < 0)
+		{
+			perror("Error:cant execute command");
+
+		}
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		wait(&status);
+		return (1);
+	}
+}
